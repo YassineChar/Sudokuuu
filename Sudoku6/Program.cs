a3e1052735b73de7879f092e2533dd757dcc4d66
Yassine Char <yassine.char@itsincom.it>
2024-01-25 17:07:57 +0100

    Creazione tabella vuota

--- Sudoku6/Program.cs
+++ Sudoku6/Program.cs
@@ -8,8 +8,26 @@ namespace Sudoku6
 {
     internal class Program
     {
+        static int[,] board = new int[9, 9];
         static void Main(string[] args)
         {
+            PrintBoard();
+            Console.ReadKey();
+        }
+        static void PrintBoard()
+        {
+            Console.WriteLine(" -----------------------");
+            for (int i = 0; i < 9; i++)
+            {
+                Console.Write("| ");
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write(board[i, j] == 0 ? "  " : $"{board[i, j]} ");
+                    if (j % 3 == 2) Console.Write("| ");
+                }
+                Console.WriteLine();
+                if (i % 3 == 2) Console.WriteLine(" -----------------------");
+            }
         }
     }
 }
