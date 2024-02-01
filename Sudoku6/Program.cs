57983a74aee6ff9892e3b745a3fa362232e216a1
Alessio Castiglioni <alessio.castiglioni@itsincom.it>
2024-02-01 14:36:14 +0100

    Aggiunte funzionalita' per l'interfaccia nel main

--- Sudoku6/Program.cs
+++ Sudoku6/Program.cs
@@ -12,11 +12,29 @@ namespace Sudoku6
         static int[,] originalBoard = new int[9, 9];
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-
-=======
             InitializeBoard();
             PrintBoard();
+            while (!IsSudokuSolved())
+            {
+                int row, col, num;
+                do
+                {
+                    Console.Write("Inserisci riga (1-9): ");
+                    row = int.Parse(Console.ReadLine()) - 1; Console.Write("Inserisci colonna (1-9): ");
+                    col = int.Parse(Console.ReadLine()) - 1;
+                    if (originalBoard[row, col] != 0)
+                    {
+                        Console.WriteLine("Impossibile modificare un numero fissato.");
+                        continue;
+                    }
+                    Console.Write("Inserisci numero (1-9): ");
+                    num = int.Parse(Console.ReadLine());
+                } while (!IsValidMove(row, col, num));
+                board[row, col] = num; PrintBoard();
+            }
+            Console.WriteLine("Complimenti, hai risolto il Sudoku!");
+
+
             Console.ReadKey();
         }
         static void PrintBoard()
@@ -34,6 +52,7 @@ namespace Sudoku6
                 if (i % 3 == 2) Console.WriteLine(" -----------------------");
             }
         }
+
         static void InitializeBoard()
         {
             board = new int[,]
@@ -48,7 +67,6 @@ namespace Sudoku6
             {0, 0, 0, 4, 1, 9, 0, 0, 5},
             {0, 0, 0, 0, 8, 0, 0, 7, 9}
             };
->>>>>>> origin
         }
     }
 }
